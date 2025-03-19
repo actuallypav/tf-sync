c5929173562ef75c4e035847e627cb979c24808a
actuallypav <professional.extras708@passinbox.com>
2025-03-19 01:54:40 +0000

    initial s3 script

--- main.tf
+++ main.tf
@@ -11,9 +11,36 @@ provider "aws" {
   region = var.region
 }
 
-#get aws account id
-data "aws_caller_identity" "current" {}
+resource "aws_s3_bucket" "terraform_state" {
+    bucket = var.state_bucket_name
+    force_destroy = false
+}
 
-output "account_id" {
-  value = data.aws_caller_identity.current.account_id
+resource "aws_s3_bucket_versioning" "enabled" {
+    bucket = aws_s3_bucket.terraform_state.id
+    versioning_configuration {
+      status = "Enabled"
+    }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
+    bucket = aws_s3_bucket.terraform_state.id
+    rule {
+        apply_server_side_encryption_by_default {
+          sse_algorithm = var.sse_algorithm
+        }
+    }
+}
+
+resource "aws_s3_bucket_public_access_block" "block_public_access" {
+    bucket = aws_s3_bucket.terraform_state.id
+
+    block_public_acls = true
+    block_public_policy = true
+    ignore_public_acls = true
+    restrict_public_buckets = true
+}
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.terraform_state.id
 }
--- variables.tf
+++ variables.tf
@@ -3,3 +3,15 @@ variable "region" {
   type        = string
   default = "eu-west-2"
 }
+
+variable "state_bucket_name" {
+    description = "The name of the s3 bucket used for storing states"
+    type = string
+    default = "terraform_state"
+}
+
+variable "sse_algorithm" {
+    description = "The default algorithm used to encrypt the S3 bucket"
+    type = string
+    default = "AES256"
+}
